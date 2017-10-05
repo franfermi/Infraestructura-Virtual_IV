1eb6e74b147d35dcd3fe2eea310ea0192c6d02e0
Francisco Fernández Millán <franfermi@correo.ugr.es>
2017-10-05 11:08:43 +0200

    Correción 3 test.py y metodosBD.py

--- SubjectsGII_Bot/test.py
+++ SubjectsGII_Bot/test.py
@@ -8,25 +8,5 @@ class TestMetodos(unittest.TestCase):
         num_asig = metodosBD.nombreAsignatura(nombAsig)
         self.assertTrue(num_asig)
 
-    """ Test que comprueba que la guia docente esta disponible """
-    def test_guia_disponible(self, nombAsig):
-        num_guia = metodosBD.guiaDocenteDisponible(nombAsig)
-        self.assertTrue(num_guia)
-
-    """ Test que comprueba que la fecha examen esta disponible """
-    def test_fechaEx_disponible(self, nombAsig):
-        num_fecha = metodosBD.fechaExamenDisponible(nombAsig)
-        self.assertTrue(num_fecha)
-
-    """ Test que comprueba el total de asignaturas disponibles """
-    def test_numAsig_disponibles(self):
-        num_asigs = metodosBD.numeroAsigDisponibles()
-        self.assertNotEqual(num_asigs, 0)
-
-    """ Test que comprueba que se muestran las asignaturas disponibles """
-    def test_mostrarAsig_disponibles(self):
-        filas = metodosBD.mostrarAsigDisponibles()
-        self.assertNotEqual(filas, 0)
-
 if __name__ == '__main__':
    unittest.main()
