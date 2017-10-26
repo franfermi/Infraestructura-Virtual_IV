4ff6b3f7ff00651e5d9a4902f865f73aa4a439f8
Francisco Fernández Millán <franfermi@correo.ugr.es>
2017-10-26 12:05:48 +0200

    Añadida la primera función a la API web

---
+++ API_web.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from SubjectsGII_Bot.funcionesDB import *
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/")
+def bienvenido():
+    return ("Bienvenido al servicio web de SubjectsGII_Bot")
+
+if __name__ == "__main__":
+    app.run(debug = True, use_reloader = True)
