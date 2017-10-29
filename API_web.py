a89acbcc3dbcab3aac5b124a65e09523a28a9f4a
Francisco Fernández Millán <franfermi@correo.ugr.es>
2017-10-29 10:31:38 +0100

    Modificación de fichero Procfile 0.5, cambio de puertos

--- API_web.py
+++ API_web.py
@@ -4,6 +4,7 @@
 #import json
 
 from flask import Flask
+import os
 
 app = Flask(__name__)
 #api = Api(app)
@@ -13,4 +14,5 @@ def bienvenido():
     return ("Bienvenido al servicio web de SubjectsGII_Bot")
 
 if __name__ == "__main__":
-    app.run(debug = True, use_reloader = True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
