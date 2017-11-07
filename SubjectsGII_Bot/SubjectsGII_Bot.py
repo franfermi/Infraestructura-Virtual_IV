f3696d8fde9e6de747969991b83260b0e0ef88e3
Francisco Fernández Millán <franfermi@correo.ugr.es>
2017-11-07 16:21:22 +0100

    Cambio de variables en funcionesDB 0.2

--- SubjectsGII_Bot/SubjectsGII_Bot.py
+++ SubjectsGII_Bot/SubjectsGII_Bot.py
@@ -2,7 +2,7 @@
 
 import os
 import telebot
-import funcionesDB
+#import funcionesDB
 
 TOKEN = os.environ['TOKEN']
 bot = telebot.TeleBot(TOKEN)
@@ -26,4 +26,4 @@ def comando_hola(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, "Hasta pronto!")
 
-bot.polling(none_stop=True) 
+bot.polling(none_stop=True)
