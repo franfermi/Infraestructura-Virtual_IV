6cee0f4f63c3ccfb31a20452685885e98d20ce30
Francisco Fernández Millán <franfermi@correo.ugr.es>
2017-12-17 17:33:07 +0100

    Vuelta a la configuración anterior

--- API_web.py
+++ API_web.py
@@ -74,6 +74,7 @@ def not_found():
 """
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
 
 
