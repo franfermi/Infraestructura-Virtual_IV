b47073310927e247a52e51188e9d3fd659816a00
Francisco Fernández Millán <franfermi@correo.ugr.es>
2017-12-17 16:39:28 +0100

    Cambio de puerto en API_web

--- API_web.py
+++ API_web.py
@@ -74,7 +74,6 @@ def not_found():
 """
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
+    app.run(host='0.0.0.0', port=80)
 
 
